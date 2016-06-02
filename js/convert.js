123dc0f37ede91a4825bacb481cf4934e869347a
aul12 <paul.nykiel@gmail.com>
2016-06-03 01:22:20 +0200

    Conversion working

--- js/convert.js
+++ js/convert.js
@@ -14,6 +14,13 @@ function convertToDec(type, text){
             return parseInt(text, 2);
         case "Hex":
             return parseInt(text, 16);
+        case "ASCII":
+            var num = 0;
+            for(var c=0; c<text.length; c++){
+                console.log(text.length-c);
+                num += (text.charCodeAt(c) * ((text.length-c))<<(c*8));
+            }
+            return num;
         default:
             alert("Error");
             return;
@@ -26,4 +33,11 @@ function textChanged(parent, type){
     $("#AreaDecimal").val(value.toString(10));
     $("#AreaBinary").val(value.toString(2));
     $("#AreaHex").val(value.toString(16));
+
+    var hex = value.toString(16);
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    
+    $("#AreaASCII").val(str);
 }
