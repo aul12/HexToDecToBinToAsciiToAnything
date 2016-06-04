cf8bf0037915b742c816cb89b732a61feaf702a1
aul12 <paul.nykiel@gmail.com>
2016-06-04 16:47:52 +0200

    Base 64, Octal, Bugfixes

--- js/addDialog.js
+++ js/addDialog.js
@@ -5,7 +5,7 @@ $(function(){
         var workspace = $("#workspace").html();
 
         workspace += "<div class=\"jumbotron\">\n    <h2>"+type+"</h2>\n    <div class=\"form-group\">\n        <textarea class=\"form-control\" " +
-            "rows=\"3\" oninput=\'textChanged(this, \""+type+"\")\' id='Area"+type+"'></textarea>\n    </div>\n</div>\'";
+            "rows=\"3\" oninput=\'textChanged(this, \""+type+"\")\' encodingType='"+type+"'></textarea>\n    </div>\n</div>\'";
 
         $("#workspace").html(workspace);
     });
--- js/convert.js
+++ js/convert.js
@@ -3,6 +3,14 @@ $(function() {
 
 var value = 0;
 
+function asciiToDec(text){
+    var num = "";
+    for(var c=0; c<text.length; c++){
+        num += text.charCodeAt(c).toString(16);
+    }
+    return parseInt(num, 16);
+}
+
 function convertToDec(type, text){
     if(text == "")
         return 0;
@@ -15,12 +23,17 @@ function convertToDec(type, text){
         case "Hex":
             return parseInt(text, 16);
         case "ASCII":
-            var num = 0;
-            for(var c=0; c<text.length; c++){
-                console.log(text.length-c);
-                num += (text.charCodeAt(c) * ((text.length-c))<<(c*8));
+            return asciiToDec(text);
+        case "Base64":
+            try{
+                return asciiToDec(atob(text));
+            }catch(e){
+                console.log("Falsch");
             }
-            return num;
+            break;
+        case "Octal":
+            return parseInt(text, 8);
+            break;
         default:
             alert("Error");
             return;
@@ -30,14 +43,20 @@ function convertToDec(type, text){
 function textChanged(parent, type){
     value = convertToDec(type, parent.value);
 
-    $("#AreaDecimal").val(value.toString(10));
-    $("#AreaBinary").val(value.toString(2));
-    $("#AreaHex").val(value.toString(16));
+    $("textarea[encodingType='Decimal']").val(value.toString(10));
+    $("textarea[encodingType='Binary']").val(value.toString(2));
+    $("textarea[encodingType='Hex']").val(value.toString(16));
+    $("textarea[encodingType='Octal']").val(value.toString(8));
+
+
 
     var hex = value.toString(16);
     var str = '';
     for (var i = 0; i < hex.length; i += 2)
         str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    
-    $("#AreaASCII").val(str);
+
+    $("textarea[encodingType='ASCII']").val(str);
+
+    if(type != "Base64")
+        $("textarea[encodingType='Base64']").val(btoa(str));
 }
