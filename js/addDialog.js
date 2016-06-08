08a05f514e8ffbe2a0f27ddac3da70d61d0d350e
aul12 <paul.nykiel@gmail.com>
2016-06-08 19:59:02 +0200

    Delete Button, fixed fab

--- js/addDialog.js
+++ js/addDialog.js
@@ -15,12 +15,16 @@ $(function(){
 });
 
 function addField(type){
-    $("#row1").append("<div class=\"jumbotron\">\n    <h2>"+type+"</h2>\n    <div class=\"form-group\">\n        <textarea class=\"form-control\" " +
-        "rows=\"3\" oninput=\'textChanged(this, \""+type+"\", 0)\' encodingType='"+type+"' row='0'></textarea>\n    </div>\n</div>\'");
+    $("#row1").append("<div class=\"jumbotron\">\n    <button type=\"button\" class=\"close\" onclick=\'removeDialog(this)\' aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n    <h2>"+type+"</h2>\n    <div class=\"form-group\">\n        <textarea class=\"form-control\" " +
+        "rows=\"3\" oninput=\'textChanged(this, \""+type+"\", 0)\' encodingType=\'"+type+"\' row=\'0\'></textarea>\n    </div>\n</div>");
 
-    $("#row2").append("<div class=\"jumbotron\">\n    <h2>"+type+"</h2>\n    <div class=\"form-group\">\n        <textarea class=\"form-control\" " +
-        "rows=\"3\" oninput=\'textChanged(this, \""+type+"\", 1)\' encodingType='"+type+"' row='1'></textarea>\n    </div>\n</div>\'");
+    $("#row2").append("<div class=\"jumbotron\">\n    <button type=\"button\" class=\"close\" onclick=\'removeDialog(this)\' aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n    <h2>"+type+"</h2>\n    <div class=\"form-group\">\n        <textarea class=\"form-control\" " +
+        "rows=\"3\" oninput=\'textChanged(this, \""+type+"\", 1)\' encodingType=\'"+type+"\' row=\'1\'></textarea>\n    </div>\n</div>");
 
-    $("#rowResult").append("<div class=\"jumbotron\">\n    <h2>"+type+"</h2>\n    <div class=\"form-group\">\n        <textarea class=\"form-control\" " +
-        "rows=\"3\" oninput=\'textChanged(this, \""+type+"\", 2)\' encodingType='"+type+"' row='2'></textarea>\n    </div>\n</div>\'");
+    $("#rowResult").append("<div class=\"jumbotron\">\n    <button type=\"button\" class=\"close\" onclick=\'removeDialog(this)\' aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n    <h2>"+type+"</h2>\n    <div class=\"form-group\">\n        <textarea class=\"form-control\" " +
+        "rows=\"3\" oninput=\'textChanged(this, \""+type+"\", 2)\' encodingType=\'"+type+"\' row=\'2\'></textarea>\n    </div>\n</div>");
+}
+
+function  removeDialog(caller){
+    caller.parentElement.remove();
 }
